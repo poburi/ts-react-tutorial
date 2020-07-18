c917194cc6381673ff161be05a05adae302b5a4b
hanheesong <shohom35@naver.com>
2020-07-18 22:53:47 +0900

    인풋 상태 관리하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Greetings from "./Greetings";
 import Count from "./Counter";
+import MyForm from "./MyForm";
 
 const App: React.FC = () => {
   const onClick = (name: string) => {
     console.log(`${name} says hello`);
   };
+
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
   return (
     <>
       <Greetings name="Poburi" onClick={onClick} />
       <Count />
+      <MyForm onSubmit={onSubmit} />
     </>
   );
 };
