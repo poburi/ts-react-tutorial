fa0424b54ce6fe5a511ede56063ff652bd45cae6
hanheesong <shohom35@naver.com>
2020-07-18 23:35:20 +0900

    리듀서 자동완성 기능

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Greetings from "./Greetings";
 import Count from "./Counter";
 import MyForm from "./MyForm";
+import CounterReducer from "./CounterReducer";
+import Reducer from "./Reducer";
 
 const App: React.FC = () => {
   const onClick = (name: string) => {
@@ -16,6 +18,8 @@ const App: React.FC = () => {
       <Greetings name="Poburi" onClick={onClick} />
       <Count />
       <MyForm onSubmit={onSubmit} />
+      <CounterReducer />
+      <Reducer />
     </>
   );
 };
